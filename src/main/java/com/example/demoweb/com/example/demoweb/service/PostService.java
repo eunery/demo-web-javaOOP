8f0f2c943b096dd18c1ed46456f582ee193614fb
eunery <phisic.cons@yandex.ru>
2021-09-04 08:52:04 +0500

    Bootstrap connect

--- src/main/java/com/example/demoweb/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/com/example/demoweb/service/PostService.java
@@ -4,6 +4,7 @@ import com.example.demoweb.model.Post;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -11,9 +12,9 @@ public class PostService {
 
     public List<Post> listAllPosts(){
         List<Post> CountPost = new ArrayList<Post>() {{
-            add(new Post("123"));
-            add(new Post("Piska"));
-            add(new Post("Java the best!1"));
+            add(new Post("123", new Date()));
+            add(new Post("Sosiska", new Date()));
+            add(new Post("Java the best!1", new Date()));
         }};
         return CountPost;
     }
