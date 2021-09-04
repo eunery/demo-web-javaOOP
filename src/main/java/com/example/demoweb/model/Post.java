f31cbed9f4e71d9950f39d9120a9f7ff0db45e27
eunery <phisic.cons@yandex.ru>
2021-09-04 09:28:55 +0500

    8.4 backend( not ended )

--- src/main/java/com/example/demoweb/model/Post.java
+++ src/main/java/com/example/demoweb/model/Post.java
@@ -6,11 +6,17 @@ public class Post {
     private String text;
     private Integer likes;
     private Date creationDate;
+    private Long id;
 
-    public Post(String text,Date creationDate){
+    public Post(String text,Date creationDate, Long id){
+        this.id = id;
         this.text = text;
         this.likes = 0;
         this.creationDate = creationDate;
+    }
+
+    public void setLikes(Integer likes) {
+        this.likes = likes;
     }
 
     public Integer getLikes() {
